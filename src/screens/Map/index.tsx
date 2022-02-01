1640b4e17d06cb6f9e833ab2a6787dab090e4961
Siarhei Karol <siarhei_karol@epam.com>
2022-02-01 18:28:20 +0300

    Add Map screen

--- src/screens/Map/index.tsx
+++ src/screens/Map/index.tsx
@@ -1,12 +1,67 @@
-import React, {FC} from 'react';
-import {Text, View} from 'react-native';
-import commonStyles from '../../commonStyles';
+import React, {FC, useEffect, useState} from 'react';
+import {View, PermissionsAndroid} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import styles from './styles';
 
 const Map: FC = () => {
+  const [region, setRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  useEffect(() => {
+    const bootstrapAsync = async () => {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+          {
+            title: 'MyApp Permission',
+            message:
+              'This App needs access to your geolocation ' +
+              'so you can the map.',
+            buttonNeutral: 'Ask Me Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK',
+          },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          Geolocation.getCurrentPosition(
+            position =>
+              setRegion({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                latitudeDelta: 0.015,
+                longitudeDelta: 0.0121,
+              }),
+            error => {
+              // ToDo: show user error
+              console.error(error.code, error.message);
+            },
+            {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+          );
+        } else {
+          // ToDo: show user error
+          console.log('Geolocation permission denied');
+        }
+      } catch (err) {
+        // ToDo: show user error
+        console.warn(err);
+      }
+    };
+
+    bootstrapAsync();
+  }, []);
+
   return (
-    <View style={[commonStyles.generalWrapper]}>
-      <Text>Map</Text>
+    <View style={styles.container}>
+      <MapView provider={PROVIDER_GOOGLE} style={styles.map} region={region}>
+        <Marker
+          coordinate={{latitude: region.latitude, longitude: region.longitude}}
+        />
+      </MapView>
     </View>
   );
 };
