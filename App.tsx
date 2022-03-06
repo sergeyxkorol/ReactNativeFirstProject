f00962399ebb37dcef8c7fc57a15306712fef731
Siarhei Karol <siarhei_karol@epam.com>
2022-03-06 23:29:32 +0300

    minor

--- App.tsx
+++ App.tsx
@@ -29,7 +29,7 @@ mainTheme.colors.background = 'white';
 const App = () => {
   const navigationRef = useNavigationContainerRef();
   const routeNameRef = useRef();
-  const routeName = () => navigationRef?.getCurrentRoute()?.name;
+  const gwtRouteName = () => navigationRef?.getCurrentRoute()?.name;
 
   return (
     <SafeAreaProvider>
@@ -37,11 +37,11 @@ const App = () => {
         theme={mainTheme}
         ref={navigationRef}
         onReady={() => {
-          routeNameRef.current = routeName();
+          routeNameRef.current = gwtRouteName();
         }}
         onStateChange={async () => {
           const previousRouteName = routeNameRef.current;
-          const currentRouteName = routeName();
+          const currentRouteName = gwtRouteName();
 
           if (previousRouteName !== currentRouteName) {
             Analytics.trackEvent('onScreenOpen', {
